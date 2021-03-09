6f7a880c17a5559155129d031a34400e399e9e2c
manci <gmancillapaz@gmail.com>
2021-03-09 18:56:41 -0500

    crear y estilizar el formulario de autenticacion

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './views/Login';
+import CrearCuenta from './views/CrearCuenta';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              title: "Iniciar Sesión",
+              headerShown: false
+            }}
+          />
+
+          <Stack.Screen
+            name="CrearCuenta"
+            component={CrearCuenta}
+            options={{
+              title: "Crear Cuenta",
+            }}
+          />
+         
+        </Stack.Navigator>
+      </NavigationContainer>
      
     </>
   );
--- views/Login.js
+++ views/Login.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import { Container, Button, Text, H1, Input, Form, Item, Toast } from 'native-base';
+import globalStyles from '../styles/global';
 
 const Login = () => {
   return (
-    <Text>Login</Text>
+    <Container style={[globalStyles.contenedor, { backgroundColor: '#E84347'}]}>
+      <View style={globalStyles.contenido}>
+        <H1 style={globalStyles.titulo}>UpTask</H1>
+
+        <Form>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input
+              placeholder="Email"
+            />
+          </Item>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input
+              secureTextEntry={true}
+              placeholder="Password"
+            />
+          </Item>
+
+          <Button
+            square
+            block
+            style={globalStyles.boton}
+          >
+            <Text style={globalStyles.botonTexto}>Iniciar Sesión</Text>
+          </Button>
+
+          <Text  style={globalStyles.enlace}>Crear Cuenta</Text>
+        </Form>
+      </View>
+    </Container>
   );
 }
 
