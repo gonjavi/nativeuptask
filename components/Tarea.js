d68773260156db4bfc4b136dc46721afd86c9109
manci <gmancillapaz@gmail.com>
2021-03-11 11:54:57 -0500

    actualizar tarea

--- components/Tarea.js
+++ components/Tarea.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, ListItem, Left, Right, Icon, Toast } from 'native-base';
+import { gql, useMutation } from '@apollo/client';
+
+const ACTUALIZAR_TAREA = gql`
+  mutation actualizarTarea($id: ID!, $input: TareaInput, $estado: boolean) {
+    actualizarTarea(id: $id, input: $input, estado: $estado) {
+      nombre
+      id
+      proyecto
+      estado
+    }
+  }
+`;
 
 const Tarea = ({ tarea }) => {
+
+  // Apollo
+  const [ actualizarTarea ] = useMutation(ACTUALIZAR_TAREA);
+
+  const { id } = tarea;
+
+  // cambiar estado de uns tarea a completa o incompleta
+  const camabiarEstado = async () => {
+     
+    try {
+      const { data } = await actualizarTarea({
+        variables:
+          id,
+          input: {
+            nombre: tarea.nombre
+          },
+          estado: !tarea.estado
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-      <ListItem>
+      <ListItem
+        onPress={ () => camabiarEstado() }
+      >
         <Left>
           <Text>{Tarea.nombre}</Text>
         </Left>
