fcc272b524d39347db58126be2cc752e8af74ac1
manci <gmancillapaz@gmail.com>
2021-03-11 08:21:51 -0500

    agregar tareas

--- views/Proyecto.js
+++ views/Proyecto.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Button, Text, H2, Content, List, Form, Item, Input, Toast  } from 'native-base';
 import globalStyles from '../styles/global';
+import { gql, useMutation } from '@apollo/client';
+
+// crear nuevas tareas
+const NUEVA_TAREA = gql`
+  mutation nuevaTarea($input: TareaInput) {
+    nuevaTarea(input: $input) {
+      nombre
+      id
+      proyecto
+      estado
+    }
+  }
+`;
 
 const Proyecto = ({ route }) => {
+  const [nombre, guardarNombre] = useState('');
+  const [mensaje, guardarMensaje] = useState(null);
+  
+  // apollo 
+  const [ nuevaTarea ] = useMutation(NUEVA_TAREA);
+
+  const handleSubmit = async () => {
+    if (nombre === '') {
+      guardarMensaje('El nombre de la tarea es obligatorio');
+      return;
+    }
+
+    // almacenar en base de datos
+    try {
+      const { data } = await nuevaTarea({
+        variables: {
+          input: {
+            nombre,
+            proyecto: route.params.id
+          }
+        }
+      });
+      guardarNombre('');
+      guardarMensaje('Tarea creada correctamente');
+
+      setTimeout(() => {
+        guardarMensaje(null);
+      }, 3000);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const mostrarAlerta = () => {
+    Toast.show({
+      text: mensaje,
+      buttonText: 'OK',
+      duration: 5000
+    });
+  }
   return (
    <Container style={[globalStyles.contenedor], { backgroundColor: '#e84347'}}>
      <Form style={{ marginHorizontal: '2.5%', marginTop: 20 }}>
        <Item inlineLabel last style={globalStyles.input}>
          <Input
           placeholder="Nombre Tarea"
+          value={nombre}
+          onChangeText={ texto => guardarNombre(texto)}
          />
        </Item>
 
@@ -16,9 +71,11 @@ const Proyecto = ({ route }) => {
         style={globalStyles.boton}
         square
         block
+        onPress={() => handleSubmit()}
        >
          <Text>Crear Tarea</Text>
        </Button>
+       {mensaje && mostrarAlerta()}
      </Form>
    </Container>
   );
